4eddb3d3214ac96448805abcb076891b7258a9d4
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-03-04 11:37:41 +0000

    added sleep

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ docker compose build
 docker exec -it php composer update --no-dev
 docker compose down
 docker compose up -d
+sleep 5
 docker exec -it php ./maho cache:f
 docker exec -it php ./maho index:reindex:all

932762fa5c7f6ac165225fdf89af795457407bd0
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-03-01 21:46:25 +0000

    Added migrate command

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ docker compose build
 docker exec -it php composer update --no-dev
 docker compose down
 docker compose up -d
-sleep 5
-docker exec -it php ./maho cache:f
+sleep 10
+docker exec -it php ./maho migrate
+docker exec -it php ./maho cache:flush
 docker exec -it php ./maho index:reindex:all
